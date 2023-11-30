60e4c46d8ed4999afa4046de6063ffb45fc472f0
Lia-Moon <juliasoaresvieira@hotmail.com>
2023-11-29 22:27:05 -0300

    solução

--- linguagem_atv3_mod54.c
+++ linguagem_atv3_mod54.c
@@ -1,45 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Uma exposição em um centro de convenção precisa contabilizar os visitantes por setores. 
-// Esses setores são representados através de uma matriz quadrada de dimensão 3. 
-// Além disso, é preciso saber no final da exposição o total de visitantes por setor, 
-// apresentando estas informação através de uma matriz quadrada, e os setores que
-// conseguiram atingir uma meta definida.
-
-/* 1- Fazer leitura de um valor de meta.
-2- Somar os valores das duas matriz.
-3- Mostrar em formato de matriz a matriz total.
-4- Escrever na tela os setores que alcançaram a meta. */
-
-
 void validarMeta(int dia1[3][3], int dia2[3][3]){
     //Começar a impementar aqui
-    int meta, metaSetorDia2;
+    int meta;
     int i = 0, j = 0;
-    int total[3][3];
+    int matrizTotal[3][3];
     
-    printf("\nDigite a meta por setor: ");
+    printf("\nDigite a meta por setor\n");
     scanf("%d", &meta);
     
     for(i = 0; i < 3; i++){
         for(j = 0; j < 3; j++){
-            total[i][j] = dia1[i][j] + dia2[i][j];
+            matrizTotal[i][j] = dia1[i][j] + dia2[i][j];
         }
     }
 
-    printf("Matriz: \n");
     for(i = 0; i < 3; i++){
-    j = 0;
-        printf("%d %d %d\n", total[i][j], total[i][j+1], total[i][j+2]);
+        for(j = 0; j < 3; j++){
+            printf("%d ", matrizTotal[i][j]);
+        }
+        printf("\n");
     }
     
     for(i = 0; i < 3; i++){
         for(j = 0; j < 3; j++){
-            if(total[i][j] >= meta){
+            if(matrizTotal[i][j] >= meta){
                 printf("Setor %d de %d alcançou a meta\n", i+1, j+1);
-            } else {
-                printf("Setor %d de %d não alcançou a meta\n", i+1, j+1);
             }
         }
     }
